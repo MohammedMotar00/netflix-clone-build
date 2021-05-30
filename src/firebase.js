d1fc64a678c7a2fa3e156075e2ff63ff9fe7243a
Mohammed Motar <mohammed.motar.mm@gmail.com>
2021-05-30 16:41:46 +0200

    netflix clone build

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDntLaUCf2SogqZDhmFQITKnCikrvqYc-I",
+  authDomain: "netflix-clone-build-4519a.firebaseapp.com",
+  projectId: "netflix-clone-build-4519a",
+  storageBucket: "netflix-clone-build-4519a.appspot.com",
+  messagingSenderId: "535483349607",
+  appId: "1:535483349607:web:972d444b7d7cc3977672c2",
+};
